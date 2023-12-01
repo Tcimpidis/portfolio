ce36693163db0a2ef7c7dc46a313d0324c4fd019
Tcimpidis <150324616+Tcimpidis@users.noreply.github.com>
2023-11-30 17:35:03 -0800

    portfolio website with css module implementation (#2)
    * portfolio website with css module implementation
    
    * remove bootstrap tsx
    
    * more clean up
    
    ---------
    
    Co-authored-by: JaminTcimpidis <Jamin.Tcimpidis@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,10 @@
-import ("./bootstrap");
+import ReactDOM from 'react-dom/client';
+
+import { App } from "./app";
+import './index.scss';
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(<App />)
