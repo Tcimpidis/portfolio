19bd42e803803d04eeb5a75bc2b27ab8bf8d928a
Tcimpidis <150324616+Tcimpidis@users.noreply.github.com>
2023-12-11 11:59:18 -0800

    Brand colors (#6)
    * use new brand logo
    
    * added brand colors and moved to nextjs

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { HomeComponent } from './home'
+
+export default function Home() {
+  return (
+    <main >
+      <HomeComponent />
+    </main>
+  )
+}
---
+++ src/app/people/page.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+
+import styles from './page.module.css';
+import { Person } from '../person';
+
+export default function People() {
+  return (
+    <div className={styles.container}>
+      <Person 
+        fullName='Jamin Tcimpidis'
+        imageUrl='/assets/profile_1.png'
+        summary='this Is a summary'
+      />
+    </div>
+  )
+}
