d480a31d9f8271cacdc439bbfef8c0c7c2cdb39c
Tcimpidis <Jamin@tcimpidis.com>
2023-12-22 23:58:01 -0800

    clean up and using localhost port instead of name:

--- src/app/portfolio/@experience/page.tsx
+++ src/app/portfolio/@experience/page.tsx
@@ -1,39 +1,14 @@
 
 import { Company } from '@/components/experience/company';
 import styles from './page.module.css';
-import { CompanyMap, CompanyNames, CompanyType, ProjectMap } from '@/api/portfolio';
-
-async function getCompanyList() {
-  const res = await fetch('http://localhost:3000/portfolio/api/company-list', {method: "get"});
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-  return res.json() as Promise<CompanyType[]>;
-}
-
-async function getCompanyData() {
-  const res = await fetch('http://localhost:3000/portfolio/api/company-data', {method: "get"});
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-  return res.json() as Promise<CompanyMap>;
-}
-
-async function getProjectData() {
-  const res = await fetch('http://localhost:3000/portfolio/api/project-data', {method: "get"});
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-  return res.json() as Promise<ProjectMap>;
-}
+import { CompanyNames } from '@/api/portfolio';
 
 
 export default async function Experience() {
-  const companies = await getCompanyList();
   return (  
     <div className={styles.container}>
       <div className={styles.heading}>Work Experience</div>
-      {companies?.map(((name, i) => 
+      {CompanyNames?.map(((name, i) => 
         <Company  companyName={name} key={i} />
       ))}
     </div>
