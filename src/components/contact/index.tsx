654efe17d99f583e23cc3788e32bf44ad6306ee7
Tcimpidis <Jamin@tcimpidis.com>
2023-12-23 01:06:50 -0800

    I think i was wrong

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -91,7 +91,7 @@ export const Contact = () => {
     subject,
     message,
   }) => {
-    await fetch('http://127.0.0.1:3000/api', {
+    await fetch('http://host.docker.internal:3000/api', {
       method: "post",
       body: JSON.stringify({
         email,
