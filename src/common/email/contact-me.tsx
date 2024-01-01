b67128d2ccd83ccc29f7f6a221660ce9b7d8e52e
Tcimpidis <Jamin@tcimpidis.com>
2024-01-01 01:48:00 -0800

    fixed email call issue

--- src/common/email/contact-me.tsx
+++ src/common/email/contact-me.tsx
@@ -1,20 +1,23 @@
 interface ContactMeEmailTemplateProps {
   fullName: string;
-  phoneNumber: string;
+  email: string;
   message: string;
 }
 
 export const ContactMeEmailTemplate: React.FC<Readonly<ContactMeEmailTemplateProps>> = ({
   fullName,
-  phoneNumber,
+  email,
   message
 }) => (
   <div>
     <h1>{fullName} wants to get in touch.</h1>
-    <div>
-      <label>Phone Number:</label>
-      <span>{phoneNumber}</span>
-    </div>
-    {message}
+    <p>
+        <label>Email:</label>
+        &nbsp;
+        <span>{email}</span>
+    </p>
+    <p>
+     {message}
+    </p>
   </div>
 );
